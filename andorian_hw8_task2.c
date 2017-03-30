88228801750215721112b8c96d4b94043f8d4ba4
braxtongreenwell <braxtongreenwell@mail.weber.edu>
2017-03-30 06:28:10 +0000

    initial commit 2

---
+++ andorian_hw8_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  andorian_hw8_task2.c
+ *       	Usage:  ./andorian_hw8_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/30/2017 06:26:51 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Braxton Greenwell (), braxtongreenwell@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
