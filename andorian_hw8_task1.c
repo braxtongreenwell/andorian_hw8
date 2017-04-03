fda1f6333de29f589396731a5dc7797f7e2f6839
braxtongreenwell <braxtongreenwell@mail.weber.edu>
2017-04-03 00:33:26 +0000

    progress commit

--- andorian_hw8_task1.c
+++ andorian_hw8_task1.c
@@ -13,16 +13,49 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
-/* Function Prototypes */
+#include <string.h>
 
+#define MAX 51
+/* Function Prototypes */
+void Usage(void);
+void StringIn(char* str1[], char* str2[]);
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	
+	char* str1[MAX];
+	char* str2[MAX];
+	//int n;
 
+	*str1 = argv[1];
+	*str2 = argv[2];
+	
+	if(argc == 3)
+	{
+		printf("<%s> <%s> \n", *str1, *str2);
+	}
+	else
+	{
+		Usage();
+		exit(2);
+	}
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(void)
+{
+ printf("Usage ./task1 <str1> <str2>\n");
+ printf("Program checks if str2 is part of str1\n");
+ exit(1);
 
+ return;
+}
+
+void StringIn(char* str1[], char* str2[])
+{
+	
+	return;
+}
 
